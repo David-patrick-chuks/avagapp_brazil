38ff8686efde37da6e22a8b2bc13e982581cb3f7
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-23 13:52:10 +0100

    Auto-commit: file change detected

--- src/pages/admin/dash/ASideBar.jsx
+++ src/pages/admin/dash/ASideBar.jsx
@@ -61,104 +61,104 @@ export default function ASideBar() {
       path: "/",
     },
   ];
-  
+
   const { toggleMobile, mobile } = useMobile()
 
   return (
 
     <>
-    {
+      {
         mobile && (<><div onClick={toggleMobile} className="w-screen lg:hidden block h-screen bg-opacity-5 z-20 absolute bg-black/50">
-            
+
         </div>
-        <IoCloseCircle onClick={() => toggleMobile()} className="size-10 absolute block top-3 right-3 z-50 text-white" />
-    </>)
-    }
-  
-  {
-                mobile && ( <div 
-                  className={`  lg:relative absolute z-50 h-screen lg:h-full flex flex-col lg:w-[22%] bg-bg items-center`}>
-                  <div>
-                    <img src="/teacher/avag.png" className="size-[110px] " alt="" />
-                  </div>
-                  <div className="h-full flex flex-col justify-between pb-2 p-2">
-                    <div className="flex flex-col gap-1">
-                      {sideLinks.map((link, id) => {
-            
-                        const hrefSegments = link?.path?.split('/');
-            
-                        return (
-                          <NavLink
-                            key={id}
-                            to={link.path}
-                            onClick={toggleMobile}
-                            // className={({isActive}) => isActive ? "my-auto flex  p-[10px] rounded-lg  bg-main-dark text-white" : "my-auto flex  p-[10px] rounded-lg  bg-transparent text-black"}
-                            className={` ${  pathSegments[3] === hrefSegments[3] && "bg-main-dark text-sm text-white"
-                              } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
-                          >
-                            <span >{link.inActive}</span>
-                            <span className="inline  ml-2 ">{link.label}</span>
-                          </NavLink>
-                        )
-                      })}
-                    </div>
-                    <div className="flex flex-col gap-1">
-                      {config.map((link, id) => (
-                        <NavLink
-                        key={id}
-                        onClick={toggleMobile}
-                          to={link.path}
-                          className={` ${location.pathname === link.path && "bg-main-dark text-sm text-white"
-                            } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
-                        >
-                          <span >{link.inActive}</span>
-                          <span className="inline  ml-2 ">{link.label}</span>
-                        </NavLink>
-                      ))}
-                    </div>
-                  </div>
-                </div>)
-  }
-    
-    <div className="hidden lg:flex flex-col w-[22%] bg-bg items-center">
-      <div>
-        <img src="/teacher/avag.png" className="size-[110px] " alt="" />
-      </div>
-      <div className="h-full flex flex-col justify-between pb-2 p-2">
-        <div className="flex flex-col gap-1">
-          {sideLinks.map((link, id) => {
+          <IoCloseCircle onClick={() => toggleMobile()} className="size-10 absolute block top-3 right-3 z-50 text-white" />
+        </>)
+      }
 
-            const hrefSegments = link?.path?.split('/');
+      {
+        mobile && (<div
+          className={`  lg:relative absolute z-50 h-screen lg:h-full flex flex-col lg:w-[22%] bg-bg items-center`}>
+          <div>
+            <img src="/teacher/avag.png" className="size-[110px] " alt="" />
+          </div>
+          <div className="h-full flex flex-col justify-between pb-2 p-2">
+            <div className="flex flex-col gap-1">
+              {sideLinks.map((link, id) => {
 
-            return (
+                const hrefSegments = link?.path?.split('/');
+
+                return (
+                  <NavLink
+                    key={id}
+                    to={link.path}
+                    onClick={toggleMobile}
+                    // className={({isActive}) => isActive ? "my-auto flex  p-[10px] rounded-lg  bg-main-dark text-white" : "my-auto flex  p-[10px] rounded-lg  bg-transparent text-black"}
+                    className={` ${pathSegments[3] === hrefSegments[3] && "bg-main-dark text-sm text-white"
+                      } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
+                  >
+                    <span >{link.inActive}</span>
+                    <span className="inline  ml-2 ">{link.label}</span>
+                  </NavLink>
+                )
+              })}
+            </div>
+            <div className="flex flex-col gap-1">
+              {config.map((link, id) => (
+                <NavLink
+                  key={id}
+                  onClick={toggleMobile}
+                  to={link.path}
+                  className={` ${location.pathname === link.path && "bg-main-dark text-sm text-white"
+                    } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
+                >
+                  <span >{link.inActive}</span>
+                  <span className="inline  ml-2 ">{link.label}</span>
+                </NavLink>
+              ))}
+            </div>
+          </div>
+        </div>)
+      }
+
+      <div className="hidden lg:flex flex-col w-[22%] bg-bg items-center">
+        <div>
+          <img src="/teacher/avag.png" className="size-[110px] " alt="" />
+        </div>
+        <div className="h-full flex flex-col justify-between pb-2 p-2">
+          <div className="flex flex-col gap-1">
+            {sideLinks.map((link, id) => {
+
+              const hrefSegments = link?.path?.split('/');
+
+              return (
+                <NavLink
+                  key={id}
+                  to={link.path}
+                  // className={({isActive}) => isActive ? "my-auto flex  p-[10px] rounded-lg  bg-main-dark text-white" : "my-auto flex  p-[10px] rounded-lg  bg-transparent text-black"}
+                  className={` ${pathSegments[3] === hrefSegments[3] && "bg-main-dark text-sm text-white"
+                    } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
+                >
+                  <span >{link.inActive}</span>
+                  <span className="inline  ml-2 ">{link.label}</span>
+                </NavLink>
+              )
+            })}
+          </div>
+          <div className="flex flex-col gap-1">
+            {config.map((link, id) => (
               <NavLink
                 key={id}
                 to={link.path}
-                // className={({isActive}) => isActive ? "my-auto flex  p-[10px] rounded-lg  bg-main-dark text-white" : "my-auto flex  p-[10px] rounded-lg  bg-transparent text-black"}
-                className={` ${  pathSegments[3] === hrefSegments[3] && "bg-main-dark text-sm text-white"
+                className={` ${location.pathname === link.path && "bg-main-dark text-sm text-white"
                   } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
               >
                 <span >{link.inActive}</span>
                 <span className="inline  ml-2 ">{link.label}</span>
               </NavLink>
-            )
-          })}
-        </div>
-        <div className="flex flex-col gap-1">
-          {config.map((link, id) => (
-            <NavLink
-              key={id}
-              to={link.path}
-              className={` ${location.pathname === link.path && "bg-main-dark text-sm text-white"
-                } my-auto flex  p-[10px] text-sm rounded-lg items-center text-black `}
-            >
-              <span >{link.inActive}</span>
-              <span className="inline  ml-2 ">{link.label}</span>
-            </NavLink>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
