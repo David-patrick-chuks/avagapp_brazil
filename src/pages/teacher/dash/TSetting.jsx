8116c6c43be409f76dd122adfa8d373ac8eb1dc9
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-28 12:33:40 +0100

    Auto-commit: file change detected

--- src/pages/teacher/dash/TSetting.jsx
+++ src/pages/teacher/dash/TSetting.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, Switch } from '@material-tailwind/react'
+import { Checkbox, Radio, Switch, Typography } from '@material-tailwind/react'
 import React, { useState } from 'react'
 import { FaHome } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
@@ -13,6 +13,37 @@ function TSetting() {
   const [rate, setRate] = useState("")
   const [recom, setRecom] = useState("")
   const [form, setForm] = useState(false)
+  const [desp, setDesp] = useState("")
+  const [title, setTitle] = useState("")
+
+  const [sendNotifyModal, setSendNotifyModal] = useState(false)
+
+  const [selectedRecipient, setSelectedRecipient] = useState(""); // Track selected checkbox
+
+  const handleSend = () => {
+    if (!desp || !title) {
+      return alert("All fields are required");
+    }
+    setSendNotifyModal(true);
+  };
+
+  const handlePublish = () => {
+    if (!selectedRecipient) {
+      return alert("Please select a user to publish");
+    }
+    setSendNotifyModal(false);
+    alert(`Notification sent to ${selectedRecipient}`);
+    resetValues()
+  };
+
+
+  const resetValues = () => {
+    setDesp(""); // Reset description
+    setTitle(""); // Reset title
+    setSelectedRecipient(""); // Reset selected recipient
+    setSendNotifyModal(false); // Close the modal
+  };
+
 
   return (
     <div className='flex overflow-x-hidden bg-main-light cursor-pointer flex-col py-3  px-2 gap-4'>
@@ -161,14 +192,104 @@ function TSetting() {
 
             </div>
             <div className={`p-4 flex flex-col gap-5 divide-y rounded-md  ${create ? "flex" : "hidden"} `}>
+              <label htmlFor="name" className='font-medium text-base text-black'>Título da Notificação
+                <input
+                  style={{ resize: "none" }} onChange={(e) => setTitle(e.target.value)} value={title} rows={6} type="text" autoComplete='off' placeholder='Título da Notificação' className='text-[#545454]  2xl:px-[18px] mt-2 lg:px-[10px] px-[7px] 2xl:placeholder:text-base lg:placeholder:text-sm text-sm 2xl:text-base placeholder:text-[#545454] border-none active:border-none outline-none bg-input rounded-lg 2xl:rounded-xl w-full py-3 2xl:py-4' id='name' />
+              </label>
               <label htmlFor="name" className='font-medium text-base text-black'>Notifications
                 <textarea
-                  style={{ resize: "none" }} rows={6} type="text" autoComplete='off' placeholder='Detalhes da notificação' className='text-[#545454]  2xl:px-[18px] mt-2 lg:px-[10px] px-[7px] 2xl:placeholder:text-base lg:placeholder:text-sm text-sm 2xl:text-base placeholder:text-[#545454] border-none active:border-none outline-none bg-input rounded-lg 2xl:rounded-xl w-full py-3 2xl:py-4' id='name' />
+                  style={{ resize: "none" }} value={desp} onChange={(e) => setDesp(e.target.value)} rows={6} type="text" autoComplete='off' placeholder='Detalhes da notificação' className='text-[#545454]  2xl:px-[18px] mt-2 lg:px-[10px] px-[7px] 2xl:placeholder:text-base lg:placeholder:text-sm text-sm 2xl:text-base placeholder:text-[#545454] border-none active:border-none outline-none bg-input rounded-lg 2xl:rounded-xl w-full py-3 2xl:py-4' id='name' />
               </label>
 
-              <p className='capitalize w-full p-3 rounded-lg bg-main-dark text-white text-center mt-5'>
+              <p onClick={handleSend} className='capitalize w-full p-3 rounded-lg bg-main-dark text-white text-center mt-5'>
                 Publicar notificações
               </p>
+              {sendNotifyModal && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+                  <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+                    <h2 className="text-lg font-bold mb-4">Selecionar Destinatário</h2>
+               <div className='flex flex-col gap-3'>
+
+               <Radio
+                      color="blue"
+                      type="radio"
+                      name="recipient"
+                      value="Professor"
+                      onChange={(e) => setSelectedRecipient(e.target.value)}
+                      label={
+                        <div>
+                          <Typography color="blue-gray" className="font-semibold">
+                            Professor
+                          </Typography>
+                          <Typography variant="small" color="gray" className="font-normal">
+                            A notificação será enviada apenas para professores
+                          </Typography>
+                        </div>
+                      }
+                      containerProps={{
+                        className: "-mt-5",
+                      }}
+                    />
+                    <Radio
+                      color="blue"
+                      type="radio"
+                      name="recipient"
+                      value="Estudante"
+                      onChange={(e) => setSelectedRecipient(e.target.value)}
+                      label={
+                        <div>
+                          <Typography color="blue-gray" className="font-semibold">
+                            Estudante
+                          </Typography>
+                          <Typography variant="small" color="gray" className="font-normal">
+                            A notificação será enviada apenas para estudantes
+                          </Typography>
+                        </div>
+                      }
+                      containerProps={{
+                        className: "-mt-5",
+                      }}
+                    />
+                    <Radio
+                      color="blue"
+                      type="radio"
+                      name="recipient"
+                      value="Ambos Professor e Estudante"
+                      onChange={(e) => setSelectedRecipient(e.target.value)}
+                      label={
+                        <div>
+                          <Typography color="blue-gray" className="font-semibold">
+                            Ambos
+                          </Typography>
+                          <Typography variant="small" color="gray" className="font-normal">
+                            A notificação será enviada para professores e estudantes
+                          </Typography>
+                        </div>
+                      }
+                      containerProps={{
+                        className: "-mt-5",
+                      }}
+                    />
+               </div>
+
+                    <div className="flex justify-end mt-3 gap-3">
+                      <button
+                        onClick={() => setSendNotifyModal(false)}
+                        className="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+                      >
+                        Cancelar
+                      </button>
+                      <button
+                        onClick={handlePublish}
+                        className="px-4 py-2 rounded-md bg-main-dark text-white hover:bg-opacity-90"
+                      >
+                        Publicar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+
             </div>
           </>
 
